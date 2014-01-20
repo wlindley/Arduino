6ee215bcaa71b5922108a3649e3b01c7fc5c0692
wlindley <walker@walkerlindley.com>
2014-01-20 11:00:34 -0800

    Changing RGB LED light color with rangefinder.

--- rangefinder_test/rangefinder_test.ino
+++ rangefinder_test/rangefinder_test.ino
@@ -1,10 +1,16 @@
 #include <NewPing.h>
+#include "RGBLed.h"
 
 const int TRIGGER_PIN = 2;
 const int ECHO_PIN = 3;
 const int MAX_DISTANCE = 200; //in centimeters
 
+const int RED_PIN = 11;
+const int GREEN_PIN = 9;
+const int BLUE_PIN = 10;
+
 NewPing sonarSensor(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+RGBLed led(RED_PIN, GREEN_PIN, BLUE_PIN);
 
 void setup() {
   Serial.begin(9600);
@@ -13,7 +19,11 @@ void setup() {
 void loop() {
   delay(50);
   unsigned int microseconds = sonarSensor.ping();
+  float distance = microseconds / (float)US_ROUNDTRIP_CM;
+  float hue = 360.f * (microseconds / (float)US_ROUNDTRIP_CM) / (float)MAX_DISTANCE;
   Serial.print("Ping: ");
-  Serial.print(microseconds / US_ROUNDTRIP_CM);
-  Serial.println("cm");
+  Serial.print(distance);
+  Serial.print("cm, Hue: ");
+  Serial.println(hue);
+  led.setHSV(hue, 1.f, 1.f);
 }
