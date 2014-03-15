cf45a3a51700d4e3df84d81e76fbb902cfdb4af4
wlindley <walker@walkerlindley.com>
2014-03-15 14:53:46 -0700

    Implementing a library to ease use of shift registers.

---
+++ libraries/ShiftRegister/ShiftRegister.cpp
@@ -0,0 +1,39 @@
+#include "ShiftRegister.h"
+
+ShiftRegister::ShiftRegister(int dataPin, int clockPin, int latchPin) : dataPin(dataPin), clockPin(clockPin), latchPin(latchPin)
+{
+	pinMode(dataPin, OUTPUT);
+	pinMode(clockPin, OUTPUT);
+	pinMode(latchPin, OUTPUT);
+}
+
+void ShiftRegister::send(bool data[], int length)
+{
+	digitalWrite(latchPin, LOW);
+
+	for (int i = 0; i < length; i++)
+	{
+		digitalWrite(clockPin, LOW);
+		digitalWrite(dataPin, data[i] ? HIGH : LOW);
+		digitalWrite(clockPin, HIGH);
+	}
+
+	digitalWrite(latchPin, HIGH);
+}
+
+void ShiftRegister::send(byte data[], int length)
+{
+	digitalWrite(latchPin, LOW);
+
+	for (int i = 0; i < length; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			digitalWrite(clockPin, LOW);
+			digitalWrite(dataPin, (data[i] & (1 << j)) ? HIGH : LOW);
+			digitalWrite(clockPin, HIGH);
+		}
+	}
+
+	digitalWrite(latchPin, HIGH);
+}
