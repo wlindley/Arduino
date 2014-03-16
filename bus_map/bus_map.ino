2d4eb1ef4f357c6046416a954f4372a1898110bd
wlindley <walker@walkerlindley.com>
2014-03-15 22:45:34 -0700

    Fixed bug causing the Arduino to not be able to read from the Linux process. Also refactored code a bit to make reuse easier.

--- bus_map/bus_map.ino
+++ bus_map/bus_map.ino
@@ -1,16 +1,36 @@
 #include "Process.h"
 
 void setup() {
+    Bridge.begin();
     Serial.begin(9600);
 }
 
 void loop() {
+    updateArrival("22710", "21");
+    delay(5000);
+    updateArrival("22710", "21E");
+    delay(5000);
+}
+
+void updateArrival(const char* stopId, const char* busId) {
+    printResult(busId, getNextArrival(stopId, busId));
+}
+
+int getNextArrival(const char* stopId, const char* busId) {
     Process p;
     p.begin("/root/onebusaway/getNextArrivalAtStop.py");
-    p.addParameter("22710");
-    p.addParameter("21");
+    p.addParameter(stopId);
+    p.addParameter(busId);
     p.run();
-    Serial.println(p.exitValue());
-    
-    delay(5000);
+    return p.exitValue();
+}
+
+void printResult(const char* busId, int minutes) {
+    Serial.print(busId);
+    Serial.print(": ");
+    if (0 > minutes || 100 <= minutes) {
+        Serial.println("--");
+    } else {
+        Serial.println(minutes);
+    }
 }
