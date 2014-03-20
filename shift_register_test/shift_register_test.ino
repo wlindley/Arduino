976c6dd484e1e7b6cd9301949a3c0d0136c956c5
wlindley <walker@walkerlindley.com>
2014-03-19 21:26:59 -0700

    Updating shift_register_test to also use SevenSegmentDisplay.

--- shift_register_test/shift_register_test.ino
+++ shift_register_test/shift_register_test.ino
@@ -4,18 +4,23 @@
 byte data;
 SevenSegmentDisplay* ssDisplay;
 ShiftRegister* shift;
+int counter = 0;
 
 void setup() {
     Serial.begin(9600);
-    data = 1;
-    ssDisplay = new SevenSegmentDisplay(&data);
+    ssDisplay = new SevenSegmentDisplay(&data, true);
     shift = new ShiftRegister(4, 2, 3);
+    ssDisplay->displayDash();
     shift->send(&data, 1);
+    delay(5000);
 }
 
 void loop() {
-    data = data << 1;
-    Serial.println(data, BIN);
+    ssDisplay->displayNumber(counter);
     shift->send(&data, 1);
     delay(2000);
+    counter++;
+    if (counter >= 10) {
+        counter = 0;
+    }
 }
