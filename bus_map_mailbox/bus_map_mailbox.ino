7aefe046f8e52a0d94e453d258dfd39ec41e5207
wlindley <walker@walkerlindley.com>
2014-08-05 20:22:03 -0700

    Mailbox implementation of the bus map.

--- bus_map_mailbox/bus_map_mailbox.ino
+++ bus_map_mailbox/bus_map_mailbox.ino
@@ -1,7 +1,14 @@
 #include <Bridge.h>
 #include <Mailbox.h>
+#include <Process.h>
 #include <math.h>
 #include "DeltaTimer.h"
+#include "SevenSegmentDisplay.h"
+#include "ShiftRegister.h"
+
+const int PIN_DATA = 4;
+const int PIN_CLOCK = 3;
+const int PIN_LATCH = 2;
 
 const float MAX_DISPLAY_TIME = 50.f; //in minutes
 
@@ -10,66 +17,131 @@ struct ArrivalData {
     String busId;
     String arrivalIndex;
     float nextArrival;
-    int pinId;
 };
 
-const int NUM_IDS = 3;
-ArrivalData arrivalData[3];
+ArrivalData arrivalData[4];
+//these are displayed in reverse order
+enum ArrivalDataIds {
+    ONE_TWENTY_EIGHT_NW,
+    ONE_TWENTY_EIGHT_SE,
+    TWENTY_ONE_EXPRESS,
+    TWENTY_ONE,
+    NUM_IDS,
+};
 
 DeltaTimer deltaTimer;
+SevenSegmentDisplay* ssDisplay;
+ShiftRegister* shifter;
+byte data;
 
 void setup() {
     Serial.begin(9600);
+    delay(5000);
     
-    initializeBusStops();
+    initializeDisplay();
     clearDisplays();
     
     Bridge.begin();
     Mailbox.begin();
     
+    initializeBusStops();
+    
     deltaTimer.updateDt();
 }
 
+void initializeDisplay() {
+    ssDisplay = new SevenSegmentDisplay(&data, true);
+    shifter = new ShiftRegister(PIN_DATA, PIN_CLOCK, PIN_LATCH);
+}
+
 void initializeBusStops() {
-    arrivalData[0].stopId = "22710";
-    arrivalData[0].busId = "21"; //northbound
-    arrivalData[0].arrivalIndex = "0";
-    arrivalData[0].pinId = 3;
+    arrivalData[TWENTY_ONE].stopId = "22710";
+    arrivalData[TWENTY_ONE].busId = "21"; //northbound
+    arrivalData[TWENTY_ONE].arrivalIndex = "0";
     
-    arrivalData[1].stopId = "22710";
-    arrivalData[1].busId = "21"; //northbound
-    arrivalData[1].arrivalIndex = "1";
-    arrivalData[1].pinId = 10;
+    arrivalData[TWENTY_ONE_EXPRESS].stopId = "22710";
+    arrivalData[TWENTY_ONE_EXPRESS].busId = "21E"; //northbound
+    arrivalData[TWENTY_ONE_EXPRESS].arrivalIndex = "0";
     
-    arrivalData[2].stopId = "22710";
-    arrivalData[2].busId = "21"; //northbound
-    arrivalData[2].arrivalIndex = "2";
-    arrivalData[2].pinId = 11;
+    arrivalData[ONE_TWENTY_EIGHT_NW].stopId = "40010";
+    arrivalData[ONE_TWENTY_EIGHT_NW].busId = "128"; //north west bound
+    arrivalData[ONE_TWENTY_EIGHT_NW].arrivalIndex = "0";
+    
+    arrivalData[ONE_TWENTY_EIGHT_SE].stopId = "36470";
+    arrivalData[ONE_TWENTY_EIGHT_SE].busId = "128"; //south east bound
+    arrivalData[ONE_TWENTY_EIGHT_SE].arrivalIndex = "0";
     
     for (int i = 0; i < NUM_IDS; i++) {
         arrivalData[i].nextArrival = NAN;
-        pinMode(arrivalData[i].pinId, OUTPUT);
+        registerForUpdates(arrivalData[i].stopId, arrivalData[i].busId, arrivalData[i].arrivalIndex);
+        requestImmediateUpdate(arrivalData[i].stopId, arrivalData[i].busId, arrivalData[i].arrivalIndex);
     }
 }
 
+void registerForUpdates(String stopId, String busId, String arrivalIndex) {
+    Serial.println("--Registering for updates--");
+    Serial.print("Stop id: ");
+    Serial.println(stopId);
+    Serial.print("Bus id: ");
+    Serial.println(busId);
+    Serial.print("Arrival index: ");
+    Serial.println(arrivalIndex);
+    
+    Process p;
+    p.begin("/root/onebusaway/registerForUpdates.py");
+    p.addParameter(stopId);
+    p.addParameter(busId);
+    p.addParameter(arrivalIndex);
+    p.run();
+}
+
+void requestImmediateUpdate(String stopId, String busId, String arrivalIndex) {
+    Serial.println("--Requesting immediate update--");
+    Serial.print("Stop id: ");
+    Serial.println(stopId);
+    Serial.print("Bus id: ");
+    Serial.println(busId);
+    Serial.print("Arrival index: ");
+    Serial.println(arrivalIndex);
+    
+    Process p;
+    p.begin("/root/onebusaway/updateMailboxWithArrivalInfo.py");
+    p.addParameter(stopId);
+    p.addParameter(busId);
+    p.addParameter(arrivalIndex);
+    p.run();
+}
+
 void clearDisplays() {
-    for (int j = 0; j < 3; j++) {
-        for (int i = 0; i < NUM_IDS; i++) {
-            analogWrite(arrivalData[i].pinId, 0);
-        }
-        delay(500);
-        for (int i = 0; i < NUM_IDS; i++) {
-            analogWrite(arrivalData[i].pinId, 255);
-        }
-        delay(500);
+    for (int i = 0; i < NUM_IDS * 2; i++) {
+        ssDisplay->displayDash();
+        shifter->send(&data, 1);
     }
 }
 
 void loop() {
     float dt = deltaTimer.updateDt();
+    decrementTimers(dt);
     checkMailbox();
     displayTimes();
-    delay(100.f);
+    delay(1000.f);
+}
+
+void decrementTimers(float dt) {
+    Serial.println("--Decrementing timers--");
+    for (int i = 0; i < NUM_IDS; i++) {
+        if (!isnan(arrivalData[i].nextArrival)) {
+            Serial.print(" decrementing ");
+            Serial.print(arrivalData[i].nextArrival);
+            Serial.print(" to ");
+            Serial.println(arrivalData[i].nextArrival - dt);
+            arrivalData[i].nextArrival -= dt;
+            if (0 > arrivalData[i].nextArrival) {
+                arrivalData[i].nextArrival = NAN;
+                requestImmediateUpdate(arrivalData[i].stopId, arrivalData[i].busId, arrivalData[i].arrivalIndex);
+            }
+        }
+    }
 }
 
 void checkMailbox() {
@@ -143,17 +215,24 @@ float getArrivalDelta(String message) {
 }
 
 void displayTimes() {
+    Serial.println("--Displaying times--");
     for (int i = 0; i < NUM_IDS; i++) {
-        //Serial.print(arrivalData[i].busId);
-        //Serial.print(": ");
+        Serial.print(arrivalData[i].busId);
+        Serial.print(": ");
         float arrivalTime = arrivalData[i].nextArrival;
         if (isnan(arrivalTime) || 0 > arrivalTime) {
-            //Serial.println("--");
-            analogWrite(arrivalData[i].pinId, 255);
+            Serial.println("--");
+            ssDisplay->displayDash();
+            shifter->send(&data, 1);
+            ssDisplay->displayDash();
+            shifter->send(&data, 1);
         } else {
             int minutes = round(arrivalTime / 60.f); //convert seconds to minutes
-            //Serial.println(minutes);
-            analogWrite(arrivalData[i].pinId, constrain((minutes / MAX_DISPLAY_TIME) * 255.f, 0, 255)); //convert minutes to 0-255
+            Serial.println(minutes);
+            ssDisplay->displayNumber(minutes % 10);
+            shifter->send(&data, 1);
+            ssDisplay->displayNumber(minutes / 10);
+            shifter->send(&data, 1);
         }
     }
 }
